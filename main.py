9be3d66f5c4e8884f680aa721bdfe24ba71dc58c
Jame <enzosuname@gmail.com>
2022-01-05 09:21:12 -0500

    woo! cards !

--- main.py
+++ main.py
@@ -1,37 +1,42 @@
-import pygame as g # in terminal -> pip install pygame
-import math as m
-import random as r
+import pygame as pg
+import sprites
+from settings import *
 
-# Create color constants
+pg.init()
 
+# Set Base Screen
+screen = pg.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+pg.display.set_caption("Card Game")
 
-# Game constants
+card = sprites.SpriteSheet("images/deck_of_cards.png")
+x_margin = 11
+y_margin = 2
+x_pad = 22
+y_pad = 4
 
-PI = m.pi
-DISPLAY_WIDTH = 1900
-DISPLAY_LENGTH = 1060
-SIZE = (DISPLAY_WIDTH,DISPLAY_LENGTH)
-FPS = 60
+ace_hearts = card.image_at((x_margin, y_margin, 43, 60))
+#card_list = card.load_grid_images(4, 14, x_margin, x_pad, y_margin, y_pad)
+print(ace_hearts)
 
-# def for similar attributes
+playing = True
 
+clock = pg.time.Clock()
 
-#########################################################
+while playing:
 
-g.init()
+   clock.tick(FPS)
 
-# game dependents
-screen = g.display.set_mode(SIZE)
-g.display.set_caption("Jame Scene")
-clock = g.time.Clock()
-# game
-running = True
-while running:
-    for event in g.event.get():
-        if event.type == g.QUIT:
-            running = False
+   for event in pg.event.get():
+       if event.type == pg.QUIT:
+           playing = False
+       if event.type == pg.KEYDOWN:    # allow for q key to quit the game
+           if event.key == pg.K_q:
+               playing == False
 
-    # screen.fill()
+   screen.fill(BLACK)
 
-    g.display.flip()
-    clock.tick(FPS)
+   screen.blit(ace_hearts, (100, 100))
+
+   pg.display.flip()
+
+pg.quit()
---
+++ settings.py
@@ -0,0 +1,14 @@
+import pygame as pg
+
+# Constants
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+YELLOW = (242, 235, 31)
+GREEN = (31, 242, 137)
+RED = (255, 0, 0)
+
+FPS = 60
+
+WIN_WIDTH = 800
+WIN_HEIGHT = 600
+TILE_SIZE = 50
